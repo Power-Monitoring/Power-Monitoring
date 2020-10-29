f7911ebe50f8ceb8019d56007b82b9e9948f577b
Galterian <ggrozdev@gmail.com>
2020-10-29 18:35:24 +0200

    add initial migration

--- Data/PowerMonitoringContext.cs
+++ Data/PowerMonitoringContext.cs
@@ -11,5 +11,26 @@ namespace Power_Monitoring.Data
     }
 
     public DbSet<User> FavoriteBands { get; set; }
-  }
+
+    #region OnConfiguring
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // In order to be able to create migrations and update database:
+        if (!options.IsConfigured)
+        {
+            options.UseSqlServer("YourLocalConnectionStringShouldBeHere");
+        }
+        base.OnConfiguring(options);
+    }
+    #endregion
+
+    #region Model Creating
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Your Model Crerating Stuff
+    }
+
+    #endregion Model Creating
+    }
 }
---
+++ Migrations/20201029163148_AddUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Power_Monitoring.Migrations
+{
+    public partial class AddUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteBands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    SecondName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteBands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteBands");
+        }
+    }
+}
